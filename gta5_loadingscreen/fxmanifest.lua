d2cb828b9c21111ed6208c0ff0b29457f206f642
Leo <83591526+M3-13@users.noreply.github.com>
2021-08-14 20:42:05 +0200

    Add files via upload

---
+++ gta5_loadingscreen/fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version 'cerulean'
+games { 'gta5' }
+
+author 'SBC-Systems (M3)'
+description 'LoadingScreen'
+version '1.0.0'
+
+loadscreen 'index.html'
+
+files {
+    'index.html',
+    'css/style.css',
+    'img/*',
+    'js/infoshow.js',
+    'js/slideshow.js',
+    'music/song.mp3'
+}
